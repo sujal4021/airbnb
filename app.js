5bc4bba4ab1e2c62697fcd7df50952013be564a6
suju4021 <2023430063@vpmthane.org>
2024-11-05 21:02:51 +0530

    edited

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs")
+const path = require("path")
 const mongoose = require("mongoose");
 const port = 3000;
 const Listing = require("./models/listing")
 const mongo_url = "mongodb://localhost:27017/airbnb";
 async function main() {
-        await mongoose.connect(mongo_url);
+    await mongoose.connect(mongo_url);
 }
 main().then(() => {
     console.log("connected to DB")
@@ -13,6 +15,9 @@ main().then(() => {
     .catch((err) => {
         console.log(err)
     })
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
     res.status(200).send("hello airbnb");
 });
@@ -30,9 +35,13 @@ app.get("/", (req, res) => {
 //  console.log("sample  was saved")
 // }) 
 
+app.get("/listings", async (req, res) => {
+    const allListing = await Listing.find({})
+    res.render("listings/index")
+})
 
-app.listen(port, (err,data) => {  
-        console.log(`Server is listenings on ${port}`);
+app.listen(port, (err, data) => {
+    console.log(`Server is listenings on ${port}`);
 });
 
 
