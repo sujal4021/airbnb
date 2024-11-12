55dacfcfc2128314cf952358acf126683fec6716
suju4021 <2023430063@vpmthane.org>
2024-11-12 20:52:57 +0530

    route file

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.get("/listings/:id", async (req, res) => {
     let { id } = req.params
     const listing = await Listing.findById(id)
     res.render("listings/show", { listing })
-})
+}) 
 
 
 
