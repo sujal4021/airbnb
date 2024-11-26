5fc521823ecaa303441a3610ab12acc5784c1d82
suju4021 <2023430063@vpmthane.org>
2024-11-26 20:41:46 +0530

    .

--- models/listing.js
+++ models/listing.js
@@ -8,7 +8,7 @@ const listingSchema = new Schema({
           required: true 
         },
     description: String,
-    image: {
+    image: { 
         type: String,
         default: "https://plus.unsplash.com/premium_photo-1676218968741-8179dd7e533f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bW91bnRhaW58ZW58MHx8MHx8fDA%3D",
         set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1676218968741-8179dd7e533f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bW91bnRhaW58ZW58MHx8MHx8fDA%3D" : v,
