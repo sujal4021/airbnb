7120d0bd846b3e9090c0c213840267a7fd3fe03f
suju4021 <2023430063@vpmthane.org>
2024-11-26 20:42:19 +0530

    .

--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,7 @@
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
-          event.preventDefault()
+          event.preventDefault() 
           event.stopPropagation()
         }
   
